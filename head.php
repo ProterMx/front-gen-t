222ff8cd50f29ced21cf37e372e1f99487a74a22
proter <gerardo@draoomedia.com>
2014-04-03 19:23:35 -0600

    primer version de Gen-t

---
+++ head.php
@@ -0,0 +1,21 @@
+<meta charset="uft-8"/>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+<link rel="stylesheet" href"css/nomalize.css"/>
+
+<link rel="stylesheet" href"css/style.css"/>
+
+<link rel="shortcut icon" href"favicon.ico"/>
+
+
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+
+<script type="text/javascript">google.load("jquery", "1.6.2");</script>
+
+<script src="js/modernizr.js"> </script>
+
+<script src="js/processing.js"> </script>
+
---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+
+<html lang="es">
+
+	<head>
+
+		<?php include("head.php"); ?>
+
+	</head>
+
+<body> 
+
+	<div class="container">
+
+		<header>Gen-T</header>
+
+		<nav>menu</nav>
+
+		<div class="contenido">
+
+ 
+		Contenido contenido chido
+
+	</div>
+
+	<aside>
+
+		aside aside
+	
+	</aside>
+
+	<footer>
+		
+		Pie de página
+		
+	</footer>
+
+
+</div> <!-- fin del container -->
+
+</body>
+
+</html>
